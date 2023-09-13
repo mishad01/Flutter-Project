9be3a77fb724cd3c9f541c5110c99e8a2e6573f1
Mishad <mishadcs2002@gmail.com>
2023-09-13 12:32:53 +0600

    given user variable

--- roll_dice_app/lib/Dice_roller.dart
+++ roll_dice_app/lib/Dice_roller.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
 
-class DiceRoller extends StatefulWidget{
+class DiceRoller extends StatefulWidget {
+  DiceRoller({super.key});
   @override
-  State <DiceRoller> createState(){
-    return
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
   }
 }
 
-class _DiceRollerState extends State<DiceRoller>{
-  @override
-  var activeImage;
-  Widget build (context){
-    return Column(children: [Image.asset(activeImage,width: 200,),],);
+class _DiceRollerState extends State<DiceRoller> {
+  var activeImage = 'assets/images/dice-1.png';
+  void rolldice() {
+    setState(() {
+      activeImage = 'assets/images/dice-4.png';
+    });
+    //activeImage = 'assets/images/dice-4.png';
+  }
+
+  Widget build(context) {
+    return Column(
+      //Its by default takes all the vartical space
+      //So if wanna make it vartically centered we use this
+      mainAxisSize: MainAxisSize.min, //using this we centered it
+      children: [
+        //Here it will take a list of widget
+        Image.asset(
+          activeImage,
+          width: 200,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          onPressed: rolldice,
+          style: TextButton.styleFrom(
+            //padding: const EdgeInsets.only(
+            //top: 20), //Padding is mainly giving space
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 25),
+          ),
+          child: const Text('Roll Dice'),
+        )
+      ],
+    );
   }
 }
--- roll_dice_app/lib/GradientContainer.dart
+++ roll_dice_app/lib/GradientContainer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/Dice_roller.dart';
 
 var a = Alignment.topCenter;
 var b = Alignment.bottomCenter;
 
+// ignore: camel_case_types
 class gradientContainer extends StatelessWidget {
   gradientContainer({super.key, required this.colors});
   List<Color> colors;
@@ -18,31 +20,7 @@ class gradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          //Its by default takes all the vartical space
-          //So if wanna make it vartically centered we use this
-          mainAxisSize: MainAxisSize.min, //using this we centered it
-          children: [
-            //Here it will take a list of widget
-            Image.asset(
-              'assets/images/dice-1.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: rolldice,
-              style: TextButton.styleFrom(
-                //padding: const EdgeInsets.only(
-                //top: 20), //Padding is mainly giving space
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 25),
-              ),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
