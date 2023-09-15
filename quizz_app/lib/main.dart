4bb0e922a1d3c4b5f23c2a1cd147c77ef25410ec
Mishad <mishadcs2002@gmail.com>
2023-09-15 11:11:40 +0600

    made screen for next page

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -1,24 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/quiz.dart';
 import 'package:quizz_app/startScreen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 112, 55, 179),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(quiz());
 }
