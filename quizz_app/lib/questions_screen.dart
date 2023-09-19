2eccd4e7930f6961fdb167882d4af7658b113f6d
Mishad <mishadcs2002@gmail.com>
2023-09-20 01:53:03 +0600

    text allign

--- quizz_app/lib/questions_screen.dart
+++ quizz_app/lib/questions_screen.dart
@@ -17,21 +17,28 @@ class _questionScreen extends State<questionScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        //Padding is the internal spacing  between content and the borders
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment:
-            CrossAxisAlignment.stretch, //The box stretched out the full screen
-        children: [
-          Text(
-            currentQuestion.txt,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 20),
-          ...currentQuestion.answers.map((item) {
-            return answerButton(txt: item, onTap: () {});
-          }),
-        ],
+      child: Container(
+        //We are using container to get mergin option
+        margin: EdgeInsets.all(
+            20), //Using this we can rezie the answet botton box size
+        child: Column(
+          //Padding is the internal spacing  between content and the borders
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment
+              .stretch, //The box stretched out the full screen
+          children: [
+            Text(
+              currentQuestion.txt,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign
+                  .center, //We want the question in center thats why we did this
+            ),
+            const SizedBox(height: 20),
+            ...currentQuestion.answers.map((item) {
+              return answerButton(txt: item, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
