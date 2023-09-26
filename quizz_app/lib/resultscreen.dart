c9835d9a7a9c60c844a753876f941b798e571c48
Mishad <mishadcs2002@gmail.com>
2023-09-26 23:15:32 +0600

    result screen

--- quizz_app/lib/resultscreen.dart
+++ quizz_app/lib/resultscreen.dart
@@ -1,8 +1,27 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:quizz_app/data/questions.dart';
 
 class resultScreen extends StatelessWidget {
   resultScreen({super.key, required this.choosenAnswer});
   final List<String> choosenAnswer;
+
+  //Here object allows all kind of values
+  List<Map<String, Object>> getsummaryData() {
+    List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswer.length; i++) {
+      //This is a dart syntax for creating a map value ({})
+      summary.add({
+        'question_index': i,
+        'question': questions[i].txt,
+        'correct-answer': questions[i].answers[0],
+        'user_answer': choosenAnswer[i]
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -14,12 +33,12 @@ class resultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Your Anwered X out of Y questions correctly'),
-            SizedBox(
+            const Text('Your Anwered X out of Y questions correctly'),
+            const SizedBox(
               height: 30,
             ),
-            Text('List of answer and questions'),
-            SizedBox(
+            const Text('List of answer and questions'),
+            const SizedBox(
               height: 30,
             ),
             TextButton(
