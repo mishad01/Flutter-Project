ae93b06d19534e6b2b3aeecd41118c06c341124d
Mishad <mishadcs2002@gmail.com>
2023-09-27 14:38:23 +0600

    Scroll Option added

--- quizz_app/lib/data/questions_summary.dart
+++ quizz_app/lib/data/questions_summary.dart
@@ -5,29 +5,34 @@ class questionsSummary extends StatelessWidget {
   final List<Map<String, Object>> summaryData;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(
-                ((data['question_index'] as int) + 1).toString()), //TypeCasting
-            Expanded(
-              child: Column(
-                children: [
-                  Text(data['question'] as String),
-                  const SizedBox(
-                    height: 5,
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                Text(((data['question_index'] as int) + 1)
+                    .toString()), //TypeCasting
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(data['question'] as String),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      // Text(data['user_Answer'] as String),
+                      // Text(data['correct_Answer'] as String), //correct_Answer HERE THIS IS WRONG AND WE WILL GET ERROR BECAUSE IN REULT SCREEN WE GAVE correct_answer THE A was small letter
+                      Text(data['user_answer'] as String),
+                      Text(data['correct_answer'] as String),
+                    ],
                   ),
-                  // Text(data['user_Answer'] as String),
-                  // Text(data['correct_Answer'] as String), //correct_Answer HERE THIS IS WRONG AND WE WILL GET ERROR BECAUSE IN REULT SCREEN WE GAVE correct_answer THE A was small letter
-                  Text(data['user_answer'] as String),
-                  Text(data['correct_answer'] as String),
-                ],
-              ),
-            ),
-          ],
-        );
-      }).toList(),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
