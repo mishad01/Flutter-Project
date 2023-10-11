554e18f57ec30b3a11d2457b752c7914807d3e43
Mishad <mishadcs2002@gmail.com>
2023-10-11 15:52:51 +0600

    first push

---
+++ expense_tracker/lib/expenses.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class expenses extends StatefulWidget {
+  expenses({super.key});
+  @override
+  State<expenses> createState() {
+    return _expenses();
+  }
+}
+
+class _expenses extends State<expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Misahd"),
+    );
+  }
+}
