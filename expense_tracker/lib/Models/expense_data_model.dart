3a392e3c917f9bf63d59b9c110a3ce7742895ab7
Mishad <mishadcs2002@gmail.com>
2023-10-11 20:41:00 +0600

    Introducing Enums

--- expense_tracker/lib/Models/expense_data_model.dart
+++ expense_tracker/lib/Models/expense_data_model.dart
@@ -2,15 +2,24 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category {
+  food,
+  trable,
+  leisure,
+  work
+} //It allows us to create custom type
+
 class expense {
   expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : string_id = uuid.v4();
 
   final String string_id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
