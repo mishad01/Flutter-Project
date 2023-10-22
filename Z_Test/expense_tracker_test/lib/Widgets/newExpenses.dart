839e65e67a74ac1dd191281d833b32925f8470ae
Mishad <mishadcs2002@gmail.com>
2023-10-22 10:48:35 +0600

    cancel button added navigator

--- Z_Test/expense_tracker_test/lib/Widgets/newExpenses.dart
+++ Z_Test/expense_tracker_test/lib/Widgets/newExpenses.dart
@@ -13,6 +13,13 @@ class _NewExpenseState extends State<NewExpense> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  @override
+  void dispose() {
+    titleController.dispose();
+    amountController.dispose();
+    super.dispose();
+  }
+
   var enteredTtile = '';
 
   void saveTitleInput(String inputValue) {
@@ -25,15 +32,33 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
-          onChanged: saveTitleInput, //To store value;
+          //onChanged: saveTitleInput, //To store value;
+          controller: titleController,
           maxLength: 50,
           decoration: const InputDecoration(label: Text('title')),
         ),
         Row(
           children: [
+            Expanded(
+              child: TextField(
+                controller: amountController,
+                decoration:
+                    InputDecoration(prefixText: '\$', label: Text('Amount')),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Cancel')),
             ElevatedButton(
                 onPressed: () {
-                  print(enteredTtile);
+                  print(titleController.text);
+                  print(amountController.value);
                 },
                 child: Text('Save Expense'))
           ],
