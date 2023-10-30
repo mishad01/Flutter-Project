cddd300a60d470fb78a678bb04e078ac8da77c5c
Mishad <mishadcs2002@gmail.com>
2023-10-30 11:01:23 +0600

    Dark Mood Added

--- expense_tracker/lib/Widget/NewExpense.dart
+++ expense_tracker/lib/Widget/NewExpense.dart
@@ -69,7 +69,7 @@ class _NewExpanse extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
