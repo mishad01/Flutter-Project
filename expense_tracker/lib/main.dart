edb93e73dd61e0fe50ad482c205ffe16b2a39451
Mishad <mishadcs2002@gmail.com>
2023-10-30 10:49:34 +0600

    Dark Mode Added

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:expense_tracker/Widget/expense.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
+var kdarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 9, 99, 125));
 void main(List<String> args) {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          cardTheme: CardTheme().copyWith(
+            color: kdarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.all(10),
+          )),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+      ),
       home: Expenses(),
     ),
   );
