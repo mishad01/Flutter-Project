7d7a56c40c01ac017a2be402199c6fa2fb9837fb
Mishad <mishadcs2002@gmail.com>
2023-11-25 01:17:02 +0600

    order added on keys

--- Z_Test/todo_test/lib/Key/Keyes.dart
+++ Z_Test/todo_test/lib/Key/Keyes.dart
@@ -1,1 +1,54 @@
-class Todo {}
+import 'package:flutter/material.dart';
+import 'package:todo_test/Key/todo.dart';
+
+class Todo {
+  Todo(this.text, this.priority);
+  String text;
+  Priority priority;
+}
+
+class keys extends StatefulWidget {
+  @override
+  State<keys> createState() {
+    return _keys();
+  }
+}
+
+class _keys extends State {
+  var order = 'asc';
+  final _todo = [
+    Todo('Learing Flutter', Priority.urgent),
+    Todo('Practice Flutter', Priority.normal),
+    Todo('Explore other course', Priority.low),
+  ];
+
+  List<Todo> get orderedTodos {
+    // Create a copy of the _todos list
+    final sortedTodos = List.of(_todo);
+
+    // Sort the copy of the todos list based on the comparison function
+    sortedTodos.sort((a, b) {
+      // Compare the text of todo 'a' with the text of todo 'b'
+      final bComesAfterA = a.text.compareTo(b.text);
+
+      // Determine the sorting order (ascending or descending) and return the result accordingly
+      return order == 'asc' ? bComesAfterA : -bComesAfterA;
+    });
+
+    // Return the sorted list of todos
+    return sortedTodos;
+  }
+
+  void changeOrder() {
+    setState(() {
+      order = order == 'asc' ? 'desc' : 'asc';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
