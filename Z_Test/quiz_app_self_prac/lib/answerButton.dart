34f6d8f4322640320d75af5473e0b4c00011c624
Mishad <mishadcs2002@gmail.com>
2023-12-01 00:17:10 +0600

    answerButton

--- Z_Test/quiz_app_self_prac/lib/answerButton.dart
+++ Z_Test/quiz_app_self_prac/lib/answerButton.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// // class answerButton extends StatelessWidget {
-// //   answerButton({super.key});
-// //   void Function() onTap;
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Column(
-// //       children: [ElevatedButton(onPressed: onPressed, child: child)],
-// //     );
-// //   }
-// // }
+class answerButton extends StatelessWidget {
+  answerButton({super.key});
+  void Function() onTap;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [ElevatedButton(onPressed: onPressed, child: child)],
+    );
+  }
+}
