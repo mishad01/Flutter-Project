d9fc7407b86f00e3f0e0eb458da071d19b2566b4
Mishad <mishadcs2002@gmail.com>
2023-12-01 00:17:43 +0600

    quiz

--- Z_Test/quiz_app_self_prac/lib/quiz.dart
+++ Z_Test/quiz_app_self_prac/lib/quiz.dart
@@ -1,42 +1,42 @@
-// import 'package:flutter/material.dart';
-// import 'package:quiz_app_self_prac/questionScreen.dart';
-// import 'package:quiz_app_self_prac/startScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:quiz_app_self_prac/questionScreen.dart';
+import 'package:quiz_app_self_prac/startScreen.dart';
 
-// class quiz extends StatefulWidget {
-//   quiz({super.key});
-//   @override
-//   State<StatefulWidget> createState() {
-//     return _quizz();
-//   }
-// }
+class quiz extends StatefulWidget {
+  quiz({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _quizz();
+  }
+}
 
-// class _quizz extends State<quiz> {
-//   var activeScreen = "start-screen";
-//   void switchScreeen() {
-//     setState(() {
-//       activeScreen = "question-screen";
-//     });
-//   }
+class _quizz extends State<quiz> {
+  var activeScreen = "start-screen";
+  void switchScreeen() {
+    setState(() {
+      activeScreen = "question-screen";
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     Widget screenwidget = startScreen(switchScreeen);
-//     if (activeScreen == "question-screen") {
-//       screenwidget = questionScreen();
-//     }
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Container(
-//           decoration: const BoxDecoration(
-//               gradient: LinearGradient(
-//             colors: [
-//               Color.fromARGB(255, 78, 13, 151),
-//               Color.fromARGB(255, 107, 15, 168),
-//             ],
-//           )),
-//           child: screenwidget,
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    Widget screenwidget = startScreen(switchScreeen);
+    if (activeScreen == "question-screen") {
+      screenwidget = questionScreen();
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168),
+            ],
+          )),
+          child: screenwidget,
+        ),
+      ),
+    );
+  }
+}
