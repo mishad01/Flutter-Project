db567b3024012a278fab13f596a4505dae176fe2
Mishad <mishadcs2002@gmail.com>
2023-12-01 12:14:16 +0600

    answerButton

--- Z_Test/quiz_app_self_prac/lib/answerButton.dart
+++ Z_Test/quiz_app_self_prac/lib/answerButton.dart
@@ -7,7 +7,23 @@ class answerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [ElevatedButton(onPressed: onTap, child: Text(answerText))],
+      children: [
+        ElevatedButton(
+            onPressed: onTap,
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(
+                  //Using this we can mig the button small and big
+                  vertical: 10,
+                  horizontal:
+                      20), //It alwos to set seperate padding for horizontal and vertical pedding mainly spacing
+              backgroundColor: Color.fromARGB(255, 34, 15, 85),
+              foregroundColor: Colors.white,
+              // maximumSize: Size(100, 100),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)),
+            ),
+            child: Text(answerText))
+      ],
     );
   }
 }
