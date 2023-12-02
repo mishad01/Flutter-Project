f09d214d370d8eadbbcaba7900ba38060678f2ca
Mishad <mishadcs2002@gmail.com>
2023-12-02 18:09:34 +0600

    expense_structure

---
+++ Z_Test/expense_tracker_prac/lib/expenses.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker_prac/expense_structure.dart';
+import 'package:flutter/material.dart';
+
+class expenses extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _expenses();
+  }
+}
+
+class _expenses extends State<expenses> {
+  final List<expenseStruc> registeredExpenses = [
+    expenseStruc(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    // expenseStruc(
+    //   title: "Cinema",
+    //   amount: 9.99,
+    //   date: DateTime.now(),
+    //   category: Category.leisure,
+    // )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker"),
+      ),
+      body: Column(
+        children: [
+          Text("The Chart"),
+          Text("Ecpense List"),
+        ],
+      ),
+    );
+  }
+}
