04e64549a08ece09974aedfc270f90cc2f9f9e19
mishad01 <mishadcs2002@gmail.com>
2023-12-16 22:29:31 +0600

    multiscreen_and_navigartion

---
+++ Z_Test/multiscreen_and_navigartion/lib/categories.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategorieScreen extends StatelessWidget {
+  CategorieScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: 
+    
+    ,);
+  }
+}
