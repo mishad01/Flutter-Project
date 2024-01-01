bcd14b245773d00f9ee62a44c0de405069042ad7
Mishad <mishadcs2002@gmail.com>
2024-01-01 14:52:40 +0600

    Widget Vs Screens

---
+++ Z_Test/multiscreen_and_navigation_prac/lib/widget/Category_Grid_Item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:multiscreen_and_navigation_prac/categories.dart';
+import 'package:multiscreen_and_navigation_prac/model/categoryModel.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return;
+  }
+}
