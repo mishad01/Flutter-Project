118ac1ad4df54b4fe37a0ba961510ff131a8c407
Mishad <mishadcs2002@gmail.com>
2024-01-02 15:07:28 +0600

    Passing Data to the Target Screen

--- Z_Test/multiscreen_and_navigation_prac/lib/Screens/categories.dart
+++ Z_Test/multiscreen_and_navigation_prac/lib/Screens/categories.dart
@@ -1,35 +1,43 @@
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:multiscreen_and_navigation_prac/Screens/meals.dart';
 import 'package:multiscreen_and_navigation_prac/data/dummyData.dart';
+import 'package:multiscreen_and_navigation_prac/model/categoryModel.dart';
 import 'package:multiscreen_and_navigation_prac/widget/Category_Grid_Item.dart';
 
 class categoriesScreen extends StatelessWidget {
   const categoriesScreen({super.key});
 
+  //This is a method named '_selectCategory' that takes a 'BuildContext' as a parameter.
   // void _selectCategory(BuildContext context) {
-  //   //Here context is not availbale globally that's why we have to use "BuildContext context". So that we can pass context as a value to push
+  //   // Using the 'Navigator' to navigate to a new screen.
   //   Navigator.push(
+  //     // The 'context' is necessary for navigation, indicating the current build context.
   //     context,
+  //     // Creating a new MaterialPageRoute for the navigation.
   //     MaterialPageRoute(
-  //       builder: (context) => MealsScreen(title: "Some title", meals: []),
+  //       // The 'builder' property is a function that returns the widget for the new screen.
+  //       builder: (context) => MealsScreen(
+  //         // Creating an instance of the 'MealsScreen' widget with specific parameters.
+  //         title:
+  //             "Some title", // Setting the title property of the 'MealsScreen'.
+  //         meals: [], // Setting the meals property of the 'MealsScreen'.
+  //       ),
   //     ),
   //   );
   // }
-  // This is a method named '_selectCategory' that takes a 'BuildContext' as a parameter.
 
-  void _selectCategory(BuildContext context) {
-    // Using the 'Navigator' to navigate to a new screen.
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    //Here context is not availbale globally that's why we have to use "BuildContext context". So that we can pass context as a value to push
     Navigator.push(
-      // The 'context' is necessary for navigation, indicating the current build context.
       context,
-      // Creating a new MaterialPageRoute for the navigation.
       MaterialPageRoute(
-        // The 'builder' property is a function that returns the widget for the new screen.
         builder: (context) => MealsScreen(
-          // Creating an instance of the 'MealsScreen' widget with specific parameters.
-          title:
-              "Some title", // Setting the title property of the 'MealsScreen'.
-          meals: [], // Setting the meals property of the 'MealsScreen'.
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -58,7 +66,8 @@ class categoriesScreen extends StatelessWidget {
               CategoryGridItem(
                   category: category,
                   onSelectCategory: () {
-                    _selectCategory(context);
+                    _selectCategory(context,
+                        category); //We can set o rpass the category as a value to select catagory
                   }),
           ]),
     );
