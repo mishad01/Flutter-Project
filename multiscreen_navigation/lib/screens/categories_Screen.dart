ed35092bc22b3852da462dadfebffd52725f97d4
Mishad <mishadcs2002@gmail.com>
2024-01-09 10:28:04 +0600

    filteredScreen Added

--- multiscreen_navigation/lib/screens/categories_Screen.dart
+++ multiscreen_navigation/lib/screens/categories_Screen.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+//import 'package:flutter/widgets.dart';
 import 'package:multiscreen_navigation/data/dummy_data.dart';
+import 'package:multiscreen_navigation/model/categories_Model.dart';
 import 'package:multiscreen_navigation/screens/meals_Screen.dart';
 import 'package:multiscreen_navigation/widgets/category_grid.dart';
 
 class categoriesScreen extends StatelessWidget {
   categoriesScreen({super.key});
 
-  void selectCategory(BuildContext context) {
+  void selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((element) => element.categories.contains(category.id))
+        .toList(); //After click if we wanna go on a specific screen we use this
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => mealsScreen(title: "title", meals: []),
+        builder: (context) => mealsScreen(
+          title: "title",
+          meals: filteredMeals,
+        ),
       ),
     );
   } // For going to the next Page
@@ -32,7 +40,7 @@ class categoriesScreen extends StatelessWidget {
               categoryGridItem(
                 category: category,
                 onSelectCategory: () {
-                  selectCategory(context);
+                  selectCategory(context, category);
                 },
               ),
           ],
