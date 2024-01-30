4708435c70cb75cd521701df59d09c42ac579df3
SAKIF RAHMAN MISHAD <mishadcs2002@gmail.com>
2024-01-30 12:26:04 +0600

    Dismissble added

--- shopping_list_app/lib/widget/grocery_list.dart
+++ shopping_list_app/lib/widget/grocery_list.dart
@@ -29,25 +29,47 @@ class _GroceryList extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItem.remove(item);
+    });
+  }
+
   Widget build(BuildContext context) {
+    Widget content = Center(
+        child: Text(
+      'No Items added yet',
+      style: TextStyle(fontSize: 15),
+    ));
+
+    if (_groceryItem.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItem.length,
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItem[index]);
+          },
+          key: ValueKey(_groceryItem[index].id),
+          child: ListTile(
+            title: Text(_groceryItem[index].name),
+            leading: Container(
+              height: 24,
+              width: 24,
+              color: _groceryItem[index].category.color,
+            ),
+            trailing: Text(_groceryItem[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.green,
         title: Text('Your Groceries'),
         actions: [IconButton(onPressed: addItem, icon: Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItem.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryItem[index].name),
-          leading: Container(
-            height: 24,
-            width: 24,
-            color: _groceryItem[index].category.color,
-          ),
-          trailing: Text(_groceryItem[index].quantity.toString()),
-        ),
-      ),
+      body: content,
     );
   }
 }
