241cabc26c7e0087429350c2b8ff7c0a6c21592a
SAKIF RAHMAN MISHAD <mishadcs2002@gmail.com>
2024-02-08 12:54:59 +0600

    Theme added

--- Z_Test/shopping_list_app_test/lib/main.dart
+++ Z_Test/shopping_list_app_test/lib/main.dart
@@ -2,8 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Text("Hello Worlds"),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 42, 51, 59),
+        ),
+      ),
+      home: const Scaffold(
+        body: Center(child: Text("Hello Worlds")),
+      ),
+    );
+  }
 }
