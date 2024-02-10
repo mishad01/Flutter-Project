b5adb9f31515a5487f2ff330d4f760685a786bca
SAKIF RAHMAN MISHAD <mishadcs2002@gmail.com>
2024-02-10 19:46:42 +0600

    A Form-aware Dropdown Button

--- Z_Test/shopping_list_app_test/lib/widget/newItem.dart
+++ Z_Test/shopping_list_app_test/lib/widget/newItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app_test/data/categories_data.dart';
 
 class NewItem extends StatefulWidget {
   NewItem({super.key});
@@ -12,7 +13,64 @@ class _NewItem extends State<NewItem> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add new Item')),
+      appBar: AppBar(
+        title: Text('Add new Item'),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantitiy'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
