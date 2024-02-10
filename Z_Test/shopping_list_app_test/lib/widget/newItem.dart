3285837dfcb502dbbef428fd45b33f9bd21c8124
SAKIF RAHMAN MISHAD <mishadcs2002@gmail.com>
2024-02-10 15:31:15 +0600

    categories_model added

---
+++ Z_Test/shopping_list_app_test/lib/widget/newItem.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+
+// class NewItem extends StatefulWidget {
+//   NewItem({super.key});
+//   @override
+//   State<NewItem> createState() {
+//     return _NewItem();
+//   }
+// }
+
+// class _NewItem extends State<NewItem> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return;
+//   }
+// }
