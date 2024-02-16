e19bb2a3f64714722af6da449ae460fa8626b85d
SAKIF RAHMAN MISHAD <mishadcs2002@gmail.com>
2024-02-16 10:49:18 +0600

    firbase plugin added

--- firelightsync_ignitionapp/lib/main.dart
+++ firelightsync_ignitionapp/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -31,7 +37,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demoooo Home Page'),
     );
   }
 }
