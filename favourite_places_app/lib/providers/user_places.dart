041a4b16dd5dc1e411811380e0c067a014e52a14
SAKIF RAHMAN MISHAD <mishadcs2002@gmail.com>
2024-02-25 00:34:39 +0600

    provider practice

--- favourite_places_app/lib/providers/user_places.dart
+++ favourite_places_app/lib/providers/user_places.dart
@@ -1,17 +1,32 @@
 import 'package:favourite_places_app/model/place.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// class UserPlacesNotfier extends StateNotifier<List<Place>> {
+//   UserPlacesNotfier() : super(const []);
+
+//   void addPlace(String title) {
+//     final newPlace = Place(title: title);
+//     state = [newPlace, ...state]; //spread old list into new list
+//   }
+// } //Now we have a method that will update the state
+
+// //Now we need to set provider
+// final UserPlacesProvider =
+//     StateNotifierProvider<UserPlacesNotfier, List<Place>>(
+//   (ref) => UserPlacesNotfier(),
+// );
 
 class UserPlacesNotfier extends StateNotifier<List<Place>> {
   UserPlacesNotfier() : super(const []);
 
-  void addPlace(String title) {
-    final newPlace = Place(title: title);
-    state = [newPlace, ...state]; //spread old list into new list
-  }
-} //Now we have a method that will update the state
+  void addPlace(String text) {
+    final newPlace = Place(title: text);
 
-//Now we need to set provider
+    state = [newPlace, ...state];
+  }
+}
+
 final UserPlacesProvider =
     StateNotifierProvider<UserPlacesNotfier, List<Place>>(
-  (ref) => UserPlacesNotfier(),
-);
+        (ref) => UserPlacesNotfier());
