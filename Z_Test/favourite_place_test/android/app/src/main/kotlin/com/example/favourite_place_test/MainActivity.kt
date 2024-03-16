6ccb7b63004f4a34ba71e183a9a1ba74d801c90a
mishad01 <mishadcs2002@gmail.com>
2024-03-16 23:34:56 +0600

    fixed

--- Z_Test/favourite_place_test/android/app/src/main/kotlin/com/example/favourite_place_test/MainActivity.kt
+++ Z_Test/favourite_place_test/android/app/src/main/kotlin/com/example/favourite_place_test/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.favourite_place_test
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
---
+++ Z_Test/favourite_place_test/lib/providers/user_places.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlaceNotifier extends StateNotifier {
+  UserPlaceNotifier() : super(const []);
+  // void addPlace
+}
