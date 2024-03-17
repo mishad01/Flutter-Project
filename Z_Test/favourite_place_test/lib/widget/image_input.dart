80bf814bdd0162d00db0ad9e32870e96198cac41
mishad01 <mishadcs2002@gmail.com>
2024-03-17 16:44:18 +0600

    Using the Device Camera For Taking Pictures

--- Z_Test/favourite_place_test/lib/widget/image_input.dart
+++ Z_Test/favourite_place_test/lib/widget/image_input.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   ImageInput({super.key});
@@ -9,23 +11,50 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInput extends State<ImageInput> {
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 1,
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-        ),
-      ),
-      height: 250,
-      width: double.infinity,
-      alignment: Alignment.center,
-      child: TextButton.icon(
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-        onPressed: () {},
-      ),
+    Widget content = TextButton.icon(
+      icon: const Icon(Icons.camera),
+      label: const Text('Take Picture'),
+      onPressed: _takePicture,
     );
+    if (_selectedImage != null) {
+      //GestureDetector: This widget detects gestures on its child widget and invokes the specified callbacks when the gesture is detected. In this case, it's looking for a tap gesture.
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.fill,
+          //width: double.infinity,
+          //height: double.infinity,
+        ),
+      );
+    }
+    return Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
+          ),
+        ),
+        height: 250,
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: content);
   }
 }
