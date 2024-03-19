a71eb0fa29fc5ba6e5f99601ae4ea966184ef43a
mishad01 <mishadcs2002@gmail.com>
2024-03-19 13:22:53 +0600

    Getting the User's Current Location

--- Z_Test/favourite_place_test/lib/widget/location_input.dart
+++ Z_Test/favourite_place_test/lib/widget/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -8,8 +9,58 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInput extends State<LocationInput> {
+  Location? _pickedLocation;
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isGettingLocation = false;
+    });
+
+    print(locationData.latitude);
+    print(locationData.longitude);
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget previewContent = Text('No Location choosen',
+        textAlign: TextAlign.center,
+        style: Theme.of(context)
+            .textTheme
+            .bodyLarge!
+            .copyWith(color: Theme.of(context).colorScheme.onBackground));
+
+    if (_isGettingLocation == true) {
+      previewContent = CircularProgressIndicator();
+    }
+
     return Column(
       children: [
         Container(
@@ -22,19 +73,13 @@ class _LocationInput extends State<LocationInput> {
           height: 180,
           width: double.infinity,
           alignment: Alignment.center,
-          child: Text(
-            'No Location choosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-          ),
+          child: previewContent,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               icon: Icon(Icons.location_on),
               label: Text('Get current Location'),
             ),
