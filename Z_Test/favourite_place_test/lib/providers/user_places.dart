d3cf68343545e5db616bd8ef4c9eaaad76196ce0
mishad01 <mishadcs2002@gmail.com>
2024-03-26 16:16:50 +0600

    Packages for Local (On-Device) Data Storage

--- Z_Test/favourite_place_test/lib/providers/user_places.dart
+++ Z_Test/favourite_place_test/lib/providers/user_places.dart
@@ -2,7 +2,7 @@ import 'package:favourite_place_test/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
 
-class UserPlaceNotifier extends StateNotifier<List<Place>> {
+/*class UserPlaceNotifier extends StateNotifier<List<Place>> {
   // Initializing the state with an empty list.
   UserPlaceNotifier() : super(const []);
   void addPlace(String title, File image, PlaceLocation location) {
@@ -21,3 +21,16 @@ final UserPlaceProvider = StateNotifierProvider<UserPlaceNotifier, List<Place>>(
 /*The StateNotifierProvider is a generic provider provided by the Riverpod library for managing state with state notifier classes.
  When you create a StateNotifierProvider,
 you essentially create a wrapper around a state notifier class that allows other parts of your application to read and interact with the state managed by that notifier.*/
+*/
+
+class UserPlaceNotifier extends StateNotifier<List<Place>> {
+  UserPlaceNotifier() : super(const []);
+  void addPlace(String title, File image, PlaceLocation location) {
+    final newPlace = Place(title: title, image: image, location: location);
+    state = [newPlace, ...state];
+  }
+}
+
+final UserPlaceProvider = StateNotifierProvider<UserPlaceNotifier, List<Place>>(
+  (ref) => UserPlaceNotifier(),
+);
