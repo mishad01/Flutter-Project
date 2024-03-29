8b18761efdc7051ab951a546ccdc3e97b9ab1ec2
mishad01 <mishadcs2002@gmail.com>
2024-03-29 14:42:05 +0600

    Removed project

--- favourite_place_test/android/app/src/main/kotlin/com/example/favourite_place_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.favourite_place_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- favourite_places_app/android/app/src/main/kotlin/com/example/favourite_places_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.favourite_places_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
