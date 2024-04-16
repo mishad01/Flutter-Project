91838721487471de6df453c8b4dcc598291589d4
mishad01 <mishadcs2002@gmail.com>
2024-04-16 23:22:34 +0600

    main

--- favourite_place/lib/main.dart
+++ favourite_place/lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Great Places',
+      title: 'Great Placess',
       theme: theme,
       home: PlacesScreen(),
     );
