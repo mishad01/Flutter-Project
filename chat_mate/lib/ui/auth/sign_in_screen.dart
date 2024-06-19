e99ec488d8e84f1a7a2cb685ce06e0af9a542fb8
mishad01 <mishadcs2002@gmail.com>
2024-06-20 02:22:58 +0600

    Sign In Screen

--- chat_mate/lib/ui/auth/sign_in_screen.dart
+++ chat_mate/lib/ui/auth/sign_in_screen.dart
@@ -1,8 +1,18 @@
+import 'package:chat_mate/ui/test.dart';
 import 'package:chat_mate/widgets/background_widgets.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +35,14 @@ class SignInScreen extends StatelessWidget {
                   height: 10,
                 ),
                 TextFormField(
+                  controller: _emailTEController,
                   decoration: InputDecoration(hintText: 'Email'),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
                 TextFormField(
+                  controller: _passwordTEController,
                   decoration: InputDecoration(hintText: 'Password'),
                 ),
                 const SizedBox(
@@ -40,19 +52,48 @@ class SignInScreen extends StatelessWidget {
                 // const SizedBox(
                 //   height: 5,
                 // ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'I already have an account',
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                )
+                const SizedBox(
+                  height: 10,
+                ),
+                RichText(
+                  text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.8),
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 0.4,
+                      ),
+                      text: "Dont Have an account? ",
+                      children: [
+                        TextSpan(
+                            text: 'Sign Up',
+                            style: TextStyle(color: Colors.black),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                _onTapSignUpButton();
+                              }),
+                      ]),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _onTapSignUpButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TestScreen(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
+    super.dispose();
   }
 }
