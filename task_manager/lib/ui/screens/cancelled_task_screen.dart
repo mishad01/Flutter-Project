626ca272aefe7e37b21095cd02d3be113aaaa0cc
mishad01 <mishadcs2002@gmail.com>
2024-06-28 02:21:53 +0600

    New Task, Complete, In Progress and Cancelled Screen added

--- task_manager/lib/ui/screens/cancelled_task_screen.dart
+++ task_manager/lib/ui/screens/cancelled_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/widgets/profile_app_bar.dart';
 
 class CancelledTaskScreen extends StatefulWidget {
   const CancelledTaskScreen({super.key});
@@ -10,6 +11,52 @@ class CancelledTaskScreen extends StatefulWidget {
 class _CancelledTaskScreenState extends State<CancelledTaskScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: profileAppBar(),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: ListTile(
+                title: Text('Title'),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Description'),
+                    Text(
+                      '12/12/2024',
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.w600),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Chip(
+                          label: Text('New'),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                        ),
+                        ButtonBar(
+                          children: [
+                            IconButton(
+                                onPressed: () {}, icon: Icon(Icons.delete)),
+                            IconButton(onPressed: () {}, icon: Icon(Icons.edit))
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- task_manager/lib/ui/screens/new_task_screen.dart
+++ task_manager/lib/ui/screens/new_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/add_new_task_screen.dart';
 import 'package:task_manager/ui/widgets/profile_app_bar.dart';
 import 'package:task_manager/ui/widgets/task_summary_card.dart';
 
@@ -13,6 +14,10 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onTapActionButton,
+        child: Icon(Icons.add),
+      ),
       backgroundColor: Colors.grey.shade300,
       appBar: profileAppBar(),
       body: Padding(
@@ -24,14 +29,47 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
               child: ListView.builder(
                 itemCount: 5,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('Title'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Description'),
-                        Text('12/12/2024'),
-                      ],
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: ListTile(
+                        title: Text('Title'),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Description'),
+                            Text(
+                              '12/12/2024',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Chip(
+                                  label: Text('New'),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16),
+                                  ),
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 2),
+                                ),
+                                ButtonBar(
+                                  children: [
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(Icons.delete)),
+                                    IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(Icons.edit))
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   );
                 },
@@ -41,6 +79,17 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
         ),
       ),
     );
+  }
+
+  void _onTapActionButton() {
+    if (mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => AddNewTaskScreen(),
+        ),
+      );
+    }
   }
 
   Widget _buildSummarySection() {
