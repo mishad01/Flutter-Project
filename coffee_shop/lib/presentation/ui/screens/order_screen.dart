96d3d85ee0f76544adfa74e6809cd99c6e53ee7f
Mishad <mishadcs2002@gmail.com>
2024-09-22 16:23:41 +0600

    Updated Order Screen

--- coffee_shop/lib/presentation/ui/screens/order_screen.dart
+++ coffee_shop/lib/presentation/ui/screens/order_screen.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_shop/presentation/ui/utils/app_colors.dart';
 import 'package:coffee_shop/presentation/ui/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
@@ -11,8 +12,11 @@ class OrderScreen extends StatefulWidget {
 
 class _OrderScreenState extends State<OrderScreen> {
   String orderOption = 'Deliver';
+
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
         appBar: const CustomAppBar(title: 'Order'),
         body: Padding(
@@ -20,6 +24,28 @@ class _OrderScreenState extends State<OrderScreen> {
           child: ListView(
             children: [
               _buildorderOption(),
+              Gap(10),
+              Container(
+                height: 120,
+                width: 315,
+                color: Colors.yellow,
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Delivery Adress',
+                      style: textTheme.titleLarge!
+                          .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Delivery Adress',
+                      style: textTheme.titleLarge!
+                          .copyWith(fontSize: 14, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ));
@@ -32,7 +58,7 @@ class _OrderScreenState extends State<OrderScreen> {
       decoration: BoxDecoration(
           color: Color(0xffF2F2F2), borderRadius: BorderRadius.circular(14)),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: ['Deliver', 'Pickup'].map(
           (e) {
             bool isSelected = orderOption == e;
@@ -42,7 +68,7 @@ class _OrderScreenState extends State<OrderScreen> {
                 setState(() {});
               },
               child: Container(
-                width: 170,
+                width: 190,
                 height: 50,
                 decoration: BoxDecoration(
                   color: isSelected ? AppColors.themeColor : Color(0xffF2F2F2),
